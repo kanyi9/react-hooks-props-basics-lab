bda55123a26fa564a9b02bc39eeb4a3ddfcef162
Peter <peterkanyi19193@gmail.com>
2024-05-13 14:01:14 +0300

    adds props basic lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,32 @@
-import React from "react";
+// // import React from "react";
 
-function About() {
+
+// import React from "react";
+// import Links from "./Links";
+
+// function About({ bio, Links }) {
+//   return (
+//     <div id="about">
+//       <h2>About Me</h2>
+//       {bio && <p>{bio}</p>}
+//       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+//       <Links github={github} linkedin={linkedin} />
+//     </div>
+//   );
+// }
+
+// export default About;
+
+import React from "react";
+import Links from "./Links";
+
+function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio && bio.length > 1 ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={links.github} linkedin={links.linkedin} />
     </div>
   );
 }
